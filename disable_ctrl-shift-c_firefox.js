aa38082a1d4599c94cd9c6495f0a96e2d717ca21
Kyle McLaughlin <k.mclaugh@outlook.com>
2024-08-05 20:20:47 -0400

    Update disable_ctrl-shift-c_firefox.js

--- disable_ctrl-shift-c_firefox.js
+++ disable_ctrl-shift-c_firefox.js
@@ -1,15 +1,18 @@
 // ==UserScript==
-// @name         Disable shortcut devtools
-// @version      2024-03-06
-// @description  Disables CTRL-Shift-C shortcut keys for opening Devtools in Firefox
+// @name         Disable DevTools Shortcut
+// @description  Disables CTRL-Shift-C shortcut for opening DevTools in Firefox.
+// @version 1.0
 // @match        *://*/*
 // ==/UserScript==
+
+// I am not the original author of this script, I found it on StackOverflow I believe.
+// I find it very useful so I am saving here on GitHub.
 
 (function() {
     'use strict';
 
-    document.body.addEventListener('keydown', function(evt){
-        if (evt.ctrlKey && evt.shiftKey && evt.key == "C"){
+    document.body.addEventListener('keydown', function(evt) {
+        if (evt.ctrlKey && evt.shiftKey && evt.key == "C") {
             // Copy the selection to the clipboard
             document.execCommand('copy');
             // Throw away this event and don't do the default stuff
@@ -19,11 +22,12 @@
     }, false);
 
     /* Intercept and check keyup events for Ctrl+Shift+C */
-    document.body.addEventListener('keyup', function(evt){
-        if (evt.ctrlKey && evt.shiftKey && evt.key == "C"){
+    document.body.addEventListener('keyup', function(evt) {
+        if (evt.ctrlKey && evt.shiftKey && evt.key == "C") {
             // Throw away this event and don't do the default stuff
             evt.stopPropagation();
             evt.preventDefault();
         }
     }, false);
+    
 })();
